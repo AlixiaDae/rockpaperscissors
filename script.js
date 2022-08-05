6240d39019738207d311f64501f7599145632cc2
AlixiaDae <lizziejones1208@gmail.com>
2022-08-05 18:18:05 +0800

    Add button click function so that playerSelection can be compared to computerChoice in a playRound function

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let playerSelection = document.getElementById("player1");
+let player1 = document.getElementById("player1");
+let btn = document.getElementById("playbtn");
 
-function computerChoice() {
+
+//no need for regular expression. just use else "please pick rock, paper, or scissors"
+
+
+let computerChoice = function() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
+let choices = ["Rock","Paper","Scissors"];
+
 //math.floor + math.random chooses from an array. make sure to multiply by the variable.length so that it can see the number of things items in the array and choose one
 
-let choices = ["Rock","Paper","Scissors"];
+btn.onclick = function() {
+    console.log(player1.value) 
+}
+
+//needed to make playerSelection into player1 to run it through the btn click function
+//now hopefully playerSelection = the result of btn.onclick function
+
+
 
 
 
 //build a rock paper scissors game
-//prompt for player's choice between rps
+//make input for player's choice between r/p/s
 //computer randomly chooses from rps
 //make function so that code knows which is the winner or loser from player and computer choice

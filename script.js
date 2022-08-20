165454189fe26432bf3f3a3e1d50c1c80f042fe6
Alixia <lizziejones1208@gmail.com>
2022-08-20 16:37:03 +0800

    Make function for playing a round with results and an event listener on each button with forEach

--- script.js
+++ script.js
@@ -4,29 +4,43 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-//Put variables for playerSelection and computerSelection inside playRound() so that getComputerChoice() will always generate a new value
+const result = document.querySelector('.result');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
-function playRound() {
-    let playerSelection = prompt("Rock, Paper, or Scissors", "").charAt(0).toUpperCase();
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let playerSelection = button.innerHTML;
     let computerSelection = getComputerChoice();
-    if (computerSelection == "Paper") { 
-        console.log("Paper vs. Paper")
-        return ("It's a Draw!");
-    } else if (computerSelection == "Rock") {
-        console.log("Paper vs. Rock")
-        return ("You Win!")
-    } else if (computerSelection == "Scissors") {
-        console.log("Paper vs. Scissors")
-        return ("You Lose!")
+    if(playerSelection == computerSelection) {
+      result.innerHTML = 'It\'s a draw!';
     } 
-}
+    
+      else if((playerSelection == 'Rock') && (computerSelection == 'Paper')) {
+      result.innerHTML = "Player wins!";
+    } else if((playerSelection == 'Rock') && (computerSelection == 'Scissors')) {
+      result.innerHTML = "Computer wins!";
+    } 
+    
+      else if((playerSelection == 'Paper') && (computerSelection == 'Rock')) {
+      result.innerHTML = "Player wins!";
+    } else if((playerSelection == 'Paper') && (computerSelection == 'Scissors')) {
+      result.innerHTML = "Computer wins!";
+    } 
 
-//function game() {
-//    for (let i = 0; i < 5; i++) {
-//        console.log(playRound());
-//    }
-//}
+    else if((playerSelection == 'Scissors') && (computerSelection == 'Paper')) {
+      result.innerHTML = "Player wins!";
+    } else if((playerSelection == 'Scissors') && (computerSelection == 'Rock')) {
+      result.innerHTML = "Computer wins!";
+    } 
+    
+    else {
+      result.innerHTML = "TBA";
+  }})
 
-game();
+})
 
 

a96143616e27304dee9832582081b26a33a9fa93
Alixia <lizziejones1208@gmail.com>
2022-08-21 13:22:33 +0800

    Add rudimentary score system

--- script.js
+++ script.js
@@ -11,35 +11,45 @@ const scissors = document.querySelector('.scissors');
 
 const buttons = document.querySelectorAll('button');
 
+let playerScore = 0;
+let computerScore = 0;
+
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     let playerSelection = button.innerHTML;
     let computerSelection = getComputerChoice();
-    if(playerSelection == computerSelection) {
+
+    
+     if(playerSelection == computerSelection) {
       result.innerHTML = 'It\'s a draw!';
     } 
     
       else if((playerSelection == 'Rock') && (computerSelection == 'Paper')) {
       result.innerHTML = "Player wins!";
+      playerScore++;
     } else if((playerSelection == 'Rock') && (computerSelection == 'Scissors')) {
       result.innerHTML = "Computer wins!";
+      computerScore++;
     } 
     
       else if((playerSelection == 'Paper') && (computerSelection == 'Rock')) {
       result.innerHTML = "Player wins!";
+      playerScore++;
     } else if((playerSelection == 'Paper') && (computerSelection == 'Scissors')) {
       result.innerHTML = "Computer wins!";
+      computerScore++;
     } 
 
     else if((playerSelection == 'Scissors') && (computerSelection == 'Paper')) {
       result.innerHTML = "Player wins!";
+      playerScore++;
     } else if((playerSelection == 'Scissors') && (computerSelection == 'Rock')) {
       result.innerHTML = "Computer wins!";
+      computerScore++;
     } 
     
-    else {
-      result.innerHTML = "TBA";
-  }})
+  })
 
 })
 
